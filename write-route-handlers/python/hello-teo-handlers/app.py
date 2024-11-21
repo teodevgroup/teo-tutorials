999a86b48bb67bf8c170f4a81a6f2bf1d89c3341
Victor Teo <victor.teo@fillmula.com>
2024-11-21 22:41:46 +0800

    fix

--- write-route-handlers/python/hello-teo-handlers/app.py
+++ write-route-handlers/python/hello-teo-handlers/app.py
@@ -1,8 +1,20 @@
-from asyncio import run
-from teo import App
- 
+from asyncio import run, get_event_loop, new_event_loop
+from teo import App, Response
+
+
 async def main():
     app = App()
+    def hello_handler(_request):
+        return Response.html("""
+            <html>
+                <head>
+                    <title>Hello, Teo handlers</title>
+                </head>
+                <body>
+                    <h1>Hello, Teo handlers!</h1>
+                </body>
+            </html>
+        """)
+    app.main_namespace().define_handler("hello", hello_handler)
     await app.run()
- 
-run(main())
+
